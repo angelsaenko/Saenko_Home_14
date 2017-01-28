e6cc1df2e9a7cb4d708b0b5a6ea06fd459ef2a7a
Anton <angel150890@gmail.com>
2017-01-28 22:07:49 +0200

    add

--- source-js/main.js
+++ source-js/main.js
@@ -1,11 +1,13 @@
 $(window).load(function() {
-    $('.flexslider').flexslider({
+    $('#slider1').flexslider({
         animation: "slide",
         animationLoop: false,
-        itemWidth:282 ,
-        itemMargin: 4,
+        itemWidth: 282 ,
+        itemMargin: 5,
+        move: 1,
 
         loop:true,
+
         margin:10,
         responsiveClass:true,
         responsive:{
@@ -29,3 +31,76 @@ $(window).load(function() {
         }
     });
 });
+
+
+$(window).load(function() {
+    $('#slider2').flexslider({
+        animation: "slide",
+        animationLoop: false,
+        itemWidth: 162,
+        itemMargin: 3,
+        move: 1,
+
+        loop:true,
+
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            886:{
+                items:3,
+                nav:false
+            },
+            1000:{
+                items:4,
+                nav:true,
+                loop:false
+            }
+        }
+    });
+});
+
+
+$(window).load(function() {
+    $('#slider3').flexslider({
+        animation: "slide",
+        animationLoop: false,
+        itemWidth: 749,
+        itemMargin: 3,
+        move: 1,
+
+        loop:true,
+
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            886:{
+                items:3,
+                nav:false
+            },
+            1000:{
+                items:4,
+                nav:true,
+                loop:false
+            }
+        }
+    });
+});
+
+
+
