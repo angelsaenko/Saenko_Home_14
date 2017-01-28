e271e70c2371a08f16024364695d4dfaed3121eb
Anton <angel150890@gmail.com>
2017-01-28 15:31:56 +0200

    add slider

--- source-js/main.js
+++ source-js/main.js
@@ -2,7 +2,30 @@ $(window).load(function() {
     $('.flexslider').flexslider({
         animation: "slide",
         animationLoop: false,
-        itemWidth: 210,
-        itemMargin: 5
+        itemWidth:282 ,
+        itemMargin: 4,
+
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            886:{
+                items:3,
+                nav:false
+            },
+            1000:{
+                items:4,
+                nav:true,
+                loop:false
+            }
+        }
     });
 });
